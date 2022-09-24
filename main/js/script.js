5943c753806db2471d9ec17371f1e603d0fa6afa
pawarts <98350667+pawarts@users.noreply.github.com>
2022-09-24 23:49:51 +0300

    Add files via upload

---
+++ main/js/script.js
@@ -0,0 +1,19 @@
+let wrapper = document.getElementById('wrapper');
+let login = document.getElementById('login');
+let signup = document.getElementById('signup');
+let back_signup = document.getElementById('back_signup');
+let back_login = document.getElementById('back_login');
+
+
+login.addEventListener('click', () => {
+    wrapper.style.left = '0';
+});
+back_login.addEventListener('click', () => {
+    wrapper.style.left = '-100vw';
+});
+signup.addEventListener('click', () => {
+    wrapper.style.left = '-200vw';
+});
+back_signup.addEventListener('click', () => {
+    wrapper.style.left = '-100vw';
+})
