b391ec69c84cf0b5e0a9e4515eafa3182ea95782
pawarts <98350667+pawarts@users.noreply.github.com>
2022-10-13 01:46:30 +0300

    Add files via upload

---
+++ TravelUkraine/nullystyle/js/keyboard.js
@@ -0,0 +1,5 @@
+$(function () {
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    $("html, body").css({ "width": w, "height": h });
+});
